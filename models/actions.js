ea2ed1a04b5651c807ae5be110560e3f6b9d7dd1
keenmaster486 <cebrock106@gmail.com>
2019-09-25 12:50:49 -0600

    clear function and arrays for category and timeOfDay

--- models/actions.js
+++ models/actions.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const actionSchema = new Schema({
-	text: {type: String, required: true},
-	category: {type: String, required: true},
-	timeOfDay: {type: String, required: true}
+	text: {type: String, required: true, unique: true},
+	category: [{type: String, required: true}],
+	timeOfDay: [{type: String, required: true}]
 });
 
 
--- models/quotes.js
+++ models/quotes.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const quotesSchema = new Schema({
-	text: {type: String, required: true, unique: false},
+	text: {type: String, required: true, unique: true},
 	author: {type: String, required: true},
-	category: {type: String, required: true}
+	category: [{type: String, required: true}]
 })
 
 module.exports = mongoose.model('Quotes', quotesSchema)
